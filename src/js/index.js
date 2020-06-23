d08a1d0fe7ab71415d8c0b308ee7006113606eb9
Artur Seu <arturseu@gmail.com>
2020-06-23 22:36:15 +0300

    mobile height fix/ pre proj

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,19 @@
-//
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-window.addEventListener("resize", () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
+// doesn't work.///////////////////////////////
+// // We listen to the resize event
+// window.addEventListener('resize', () => {
+//   // We execute the same script as before
+//   let vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty('--vh', `${vh}px`);
+// });
+
+// window.addEventListener("load", function () {
+//   window.scrollTo(0, 0);
+// });
 
 // ОФОРМЛЯЕМ НАВИГАЦИОННОЕ МЕНЮ
 let nav = $(".navbar-nav__item a");
